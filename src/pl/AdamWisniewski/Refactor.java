001d533fe37ec4f5e6fac4b5ef333ccb955c37e3
AdamWisniewskiPC <awisniewski27@gmail.com>
2018-03-08 18:56:45 +0100

    added method - converting from string to HashMap - messageMap

--- src/pl/AdamWisniewski/Refactor.java
+++ src/pl/AdamWisniewski/Refactor.java
@@ -1,6 +1,27 @@
 package pl.AdamWisniewski;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Refactor {
+
+	// convert strings to map or edit database to push HashMap instead of Strings
+
+	public HashMap<String, String> convertMessageToHashMap(<header from MQ>, <message from MQ>) {
+		
+		List<String> headerFields = header.split("|");
+		
+		List<String> messageFields = message.split("|");
+				
+		Map<String, String> messageMap = new HashMap<String, String>();
+		
+		for (int i = 0; i < messageFields.size(); i++) {
+			messageMap.put(headerFields[0], messageFields[0]);
+		}
+		
+		return messageMap;
+	}
+	
 
 	String message = <message from MQ>;
 	String[] messageFields = message.split('|');
